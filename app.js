ed2138346a4f914ecf8aa0b1f5d5673236f897d6
Luk1Pr0 <lpytel16@gmail.com>
2020-12-29 17:12:51 +0100

    Expenses are now being saved to local storage and expense object and expenses array is no being populated

--- app.js
+++ app.js
@@ -8,14 +8,22 @@ const totalExpense = document.getElementById('total-expense');
 
 let date = "";
 let value = 0;
+let defaultId = 0;
 let expenseObj = {};
+let expenses = [];
 
 // Get input from the form
 function getInput(e) {
     e.preventDefault();
     if (datePicker.value || expense.value) {
+        // Get values of date and expense
         date = datePicker.value;
         value = expense.value;
+        // Set keys and their values for the expense object
+        expenseObj.date = date;
+        expenseObj.expense = value;
+        // Append the expense Object to the expenses
+        expenses.push(expenseObj);
         updateDOM();
     }
     datePicker.value = '';
@@ -24,58 +32,52 @@ function getInput(e) {
 
 // Append the element with the date and expense to DOM
 function updateDOM() {
+    // Create elements that need to be appended to DOM
     const expenseCard = document.createElement('div');
     const header2 = document.createElement('h2');
     const header3 = document.createElement('h3');
+    // Crete data
     expenseCard.classList.add('expense-card');
     header2.textContent = `£${value}`;
     header3.textContent = date;
+    // Append data to the expense card and expense card to the container
     expenseCard.append(header2, header3);
     expenseContainer.appendChild(expenseCard);
+    updateLocalStorage();
 }
 
+// Append expense array to localStorage
+function updateLocalStorage() {
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+}
+
+// Update DOM with local storage 
+function updateFromLocalStorage() {
+    if (localStorage.getItem('expenses')) {
+        expenses = JSON.parse(localStorage.getItem('expenses'));
+        // console.log('expense array', expenses);
+        rebuildDOM();
+    }
+}
+
+// Rebuild DOM with the local storage items
+function rebuildDOM() {
+    expenses.forEach(expense => {
+        // Create elements that need to be appended to DOM
+        const expenseCard = document.createElement('div');
+        const header2 = document.createElement('h2');
+        const header3 = document.createElement('h3');
+        // Crete data and add class to the card
+        expenseCard.classList.add('expense-card');
+        header2.textContent = `£${expense.expense}`;
+        header3.textContent = expense.date;
+        // Append data to the expense card and expense card to the container
+        expenseCard.append(header2, header3);
+        expenseContainer.appendChild(expenseCard);
+    });
+}
+
+// Get items from local storage
+updateFromLocalStorage();
+
 expenseForm.addEventListener('submit', getInput);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const hello = "THIS IS MY TEST TEXT";
-
-// function cloneExpenseCard(e, date, expense) {
-//     // Prevent page refresh on submit
-//     e.preventDefault();
-//     let cardClone = expenseCard.cloneNode(true);
-//     expenseContainer.appendChild(cardClone);
-// }
-
-// // Add date and expense value into element
-// function modifyData(e) {
-//     e.preventDefault();
-//     let expenseValue = inputValue.value;
-//     let dateValue = dateSelector.value;
-//     if (dateValue.length && expenseValue.length) {
-//         amountSpent.textContent = expenseValue;
-//         dateSpent.textContent = dateValue;
-//         cloneExpenseCard(e, expenseValue, dateValue);
-//         // console.log(dateValue, expenseValue);
-//     } else {
-//         alert("Please fill in the date and expense amount");
-//     }
-//     inputValue.value = "";
-//     dateSelector.value = "";
-// }
-
-// submitButton.addEventListener("click", modifyData);
